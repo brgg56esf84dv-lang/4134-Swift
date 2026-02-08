d8280b2c13a03a3c09b59c15eb0c6f0b28a5f48e
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-02-08 12:22:05 +0200

    init

---
+++ Planaro/App/PlanaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanaroApp.swift
+//  Planaro
+//
+//  Created by Serhii Babchuk on 08.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct PlanaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PLaunchView()
+        }
+    }
+}
